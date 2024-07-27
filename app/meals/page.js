e7f2555d1b9a5b39cba874ba9a395e5957450ad2
hamzashahid-10p <hamza.shahid@10pearls.com>
2024-07-27 20:22:26 +0500

    Add error handling

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,34 @@
+import { Suspense } from 'react';
+import Link from 'next/link';
+
+import classes from './page.module.css';
+import MealsGridPage from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import MealsLoadingPage from './loading-out';
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGridPage meals={meals} />
+}
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Ready to make some delicious meals?
-      </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>Declicious meals, created{' '} <span className={classes.highlight}>by you</span></h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 }
