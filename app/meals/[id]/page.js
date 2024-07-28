4eeaa6d1284312cc6451d65aa5f90d0f446019e0
hamzashahid-10p <hamza.shahid@10pearls.com>
2024-07-28 08:48:40 +0500

    Load Meals from DB & Add Not Found Page

--- app/meals/[id]/page.js
+++ app/meals/[id]/page.js
@@ -1,10 +1,35 @@
-export default function Home({params}) {
-  console.log('params :', params);
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+
+export default function Home({ params }) {
+  const meal = getMeal(params.id);
+
+  if (!meal) {
+    notFound();
+  }
+  
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Hello world! {params.id}
-      </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a></p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions,
+        }}>
+        </p>
+      </main>
+    </>
   );
 }
