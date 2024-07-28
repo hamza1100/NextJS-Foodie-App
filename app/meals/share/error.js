5e74fd85eea927505869c12b02450b1446ca8a66
hamzashahid-10p <hamza.shahid@10pearls.com>
2024-07-28 12:03:02 +0500

    Handle form submittion state

---
+++ app/meals/share/error.js
@@ -0,0 +1,14 @@
+'use client'
+
+export default function ErrorPage() {
+  return (
+    <>
+      <main className="error">
+        <h1>
+          An error occured!
+        </h1>
+        <p>Failed to create meal.</p>
+      </main>
+    </>
+  )
+}
